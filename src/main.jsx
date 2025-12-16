ea4a6a06e0fe140207231fc1b3bfea5a02289add
Lalit Baldaniya <lalit.baldaniya92@gmail.com>
2025-12-16 22:54:21 +0530

    second deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+
 import { ClerkProvider } from '@clerk/clerk-react'
+import { HashRouter } from 'react-router-dom'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -14,8 +15,8 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   </ClerkProvider>,
 )
