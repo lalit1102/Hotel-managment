7a54dc3d496711c1e67d9ef8e105ed345506d8d4
Lalit Baldaniya <lalit.baldaniya92@gmail.com>
2026-01-11 15:29:39 +0530

    final done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
 import MyBookings from './pages/MyBookings'
 import HotelReg from './components/HotelReg'
+import Layout from './pages/hotelOwner/Layout'
+import Dashboard from './pages/hotelOwner/Dashboard'
+import AddRoom from './pages/hotelOwner/AddRoom'
+import ListRoom from './pages/hotelOwner/ListRoom'
 function App() {
   
 const isOwnerpath = useLocation().pathname.includes("owner")
@@ -22,6 +26,11 @@ const isOwnerpath = useLocation().pathname.includes("owner")
        <Route path='/rooms' element={<AllRooms />} />
        <Route path='/rooms/:id' element={<RoomDetails />} />
        <Route path="/my-bookings" element={<MyBookings />} />
+       <Route path="/owner" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="add-room" element={<AddRoom />} />
+        <Route path="list-room" element={<ListRoom />} />
+       </Route>
       </Routes>
      </div>
     
